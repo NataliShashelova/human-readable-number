76f5f76a8848a83d2feb25e49ffd692430996afb
NataliShashelova <natalishashelova1993@gmail.com>
2023-01-27 16:03:40 +0100

    feat:add docs human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable(number) {
 	const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-	const tens = ['', '', 'twenty', 'thirty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 	const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
 
@@ -18,11 +18,10 @@ module.exports = function toReadable(number) {
 	if (number == 0) { return "zero" }
 	if (number < 10) { return ones[number] }
 	if (number % 100 === 0) {
-		return ones[Math.floor(number / 100)] + "hundred"
+		return ones[Math.floor(number / 100)] + " hundred"
 	}
-
 	if (number > 99) {
-		return ones[Math.floor(number / 100)] + "hundred" + convert_tens(number % 100);
+		return ones[Math.floor(number / 100)] + " hundred " + convert_tens(number % 100);
 	}
 	if (number >= 10 && number < 20) { return teens[number - 10] }
 	else if (number % 10 === 0) {
@@ -31,4 +30,7 @@ module.exports = function toReadable(number) {
 	else {
 		return tens[Math.floor(number / 10)] + " " + ones[number % 10];
 	}
+
+
+
 }
