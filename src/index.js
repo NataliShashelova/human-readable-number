a576b9ab196d408eb3e3a2de1af1f5746f3177a3
NataliShashelova <natalishashelova1993@gmail.com>
2023-01-27 15:58:01 +0100

    feat: add docs human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+	const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	const tens = ['', '', 'twenty', 'thirty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+
+	function convert_tens(number) {
+		if (number < 10) return ones[number];
+		else if (number >= 10 && number < 20) { return teens[number - 10] }
+		else if (number % 10 === 0) {
+			return tens[Math.floor(number / 10)]
+		}
+		else {
+			return tens[Math.floor(number / 10)] + " " + ones[number % 10];
+		}
+	}
+
+	if (number == 0) { return "zero" }
+	if (number < 10) { return ones[number] }
+	if (number % 100 === 0) {
+		return ones[Math.floor(number / 100)] + "hundred"
+	}
+
+	if (number > 99) {
+		return ones[Math.floor(number / 100)] + "hundred" + convert_tens(number % 100);
+	}
+	if (number >= 10 && number < 20) { return teens[number - 10] }
+	else if (number % 10 === 0) {
+		return tens[Math.floor(number / 10)]
+	}
+	else {
+		return tens[Math.floor(number / 10)] + " " + ones[number % 10];
+	}
 }
